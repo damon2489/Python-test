af45e0b5511d9a327e4b51e8ad8a9a6125e245e7
Damon <gordond@wsc.school.nz>
2026-05-07 12:21:47 +1200

    edited

--- Lesson 2 Data types and Variables/madlib.py
+++ Lesson 2 Data types and Variables/madlib.py
@@ -2,16 +2,16 @@
 # then output a madlib using those words.
 
 # Ask user for a name and save it in a variable
-name = input("Enter a name: ")
+name = input("A name: ")
 
 # Ask user for an animal and save it in a variable
-animal = input("Enter an animal: ")
+animal = input("An animal: ")
 
 # Ask user for a colour and save it in a variable
-colour = input("Enter a colour: ")
+colour = input("A colour: ")
 
 # Ask user for an object and save it in a variable
-object = input("Enter an object: ")
+object = input("An object: ")
 
 # Print your madlib using the 4 variables above.
 print(name + " the " + colour + " " + animal + " found a " + object + ".")
