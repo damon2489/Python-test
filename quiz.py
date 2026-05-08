7ddef171c0d322583496b091a808654a98bdd8ab
Damon <gordond@wsc.school.nz>
2026-05-08 12:59:38 +1200

    making quiz

---
+++ quiz.py
@@ -0,0 +1,18 @@
+#Animal quiz
+print("Welcome to the animal quiz!")
+
+#Question 1
+answer1 = input("What is the largest land animal? ")
+if answer1.lower() == "elephant":
+    print("Correct!")
+else:
+    print("Incorrect, the correct answer was elephant")
+
+#Question 2
+answer2 = input("What is the largest reptile? ")
+if answer2.lower() == "crocodile":
+    print("Correct!")
+else:
+    print("Incorrect, the correct answer was crocodile")
+
+#Question 3
