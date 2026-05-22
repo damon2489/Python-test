6c33df59ef2f265ff439b75db9dabf9a56903ff4
Damon <gordond@wsc.school.nz>
2026-05-22 12:57:13 +1200

    Making quiz

--- quiz.py
+++ quiz.py
@@ -3,16 +3,44 @@ print("Welcome to the animal quiz!")
 
 #Question 1
 answer1 = input("What is the largest land animal? ")
-if answer1.lower() == "elephant":
+if answer1.lower().strip() == "elephant":
     print("Correct!")
 else:
     print("Incorrect, the correct answer was elephant")
 
 #Question 2
 answer2 = input("What is the largest reptile? ")
-if answer2.lower() == "crocodile":
+if answer2.lower().strip() == "crocodile":
     print("Correct!")
 else:
     print("Incorrect, the correct answer was crocodile")
 
 #Question 3
+answer3 = input("What is the largest mammal? ")
+if answer3.lower().strip() == "blue whale":
+    print("Correct!")
+else:
+    print("Incorrect, the correct answer was blue whale")
+
+#Question 4
+answer4 = input("What is the largest bird? ") 
+if answer4.lower().strip() == "ostrich":
+    print("Correct!")
+else:
+    print("Incorrect, the correct answer was ostrich")
+
+#Question 5
+answer5 = input("What is the strongest animal relative to its size? ")
+if answer5.lower().strip() == "dung beetle":
+    print("Correct!")   
+else:
+    print("Incorrect, the correct answer was dung beetle")
+
+#Question 6
+answer6 = input("What is the fastest land animal? ")
+if answer6.lower().strip() == "cheetah":
+    print("Correct!")
+else:
+    print("Incorrect, the correct answer was cheetah")
+    
+
