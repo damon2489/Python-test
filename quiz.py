d48e54aba531a9dc0a5c4f80e0f43b0d8ccd2ffb
Damon <gordond@wsc.school.nz>
2026-06-28 18:29:19 +1200

    possibly finished quiz
    maybe finished quiz. just need to test and if anything is wrong then debug

--- quiz.py
+++ quiz.py
@@ -1,52 +1,76 @@
-#Animal quiz
-from py_compile import main
+"""
+This is an animal quiz i made for a school project.
+It asks 14 questions about animals
+shows score,
+asks user if they want to know answers,
+asks them if they want to play again.
+"""
+
+#imports random for the random shuffling of questions
 import random
+
 #sets score to zero at start
-score=0
-print("Welcome to the animal quiz!")
-print("You will be asked 14 questions about animals")
+def main():
+    while True:
+        score = 0
+        print("Welcome to the animal quiz!")
+        print("You will be asked 14 questions about animals")
 
-#dictionary with question and answer
-questions = [
-    {"question": "What is the largest land animal?", "answer": ["Elephant"]},
-    {"question": "What is the largest reptile?", "answer": ["Crocodile"]},
-    {"question": "What is the largest mammal?", "answer": ["Blue whale"]},
-    {"question": "What is the largest bird?", "answer": ["Ostrich"]},
-    {"question": "What is the strongest animal relative to its size?", "answer": ["Dung beetle", "Beetle"]},
-    {"question": "What is the fastest land animal?", "answer": ["Cheetah"]},
-    {"question": "What animal has 3 hearts?", "answer": ["Octopus", "Squid", "Cuttlefish"]},
-    {"question": "What is the sleepiest animal?", "answer": ["Koala"]},
-    {"question": "What is the only bird that can fly backwards?", "answer": ["Hummingbird"]},
-    {"question": "What is the only mammal capable of flight?", "answer": ["Bat"]},
-    {"question": "Out of the following 4 animals, which can hold their breath the longest underwater? A) Dolphin B) Orca C) Seal D) Sloth", "answer": ["D", "Sloth", "D) Sloth"]},
-    {"question": "What animal laughs when tickled?", "answer": ["Rat"]},
-    {"question": "What is the only animal to have cube shaped poop?", "answer": ["Wombat"]},
-    {"question": "What mammal has the strongest bite force?", "answer": ["Hippopotamus", "Hippo"]}
-]
-
-
-#shuffles the question so they are in a random order every time
-random.shuffle(questions)
+        #dictionary with question and answer
+        questions = [
+            {"question": "What is the largest land animal?", "answer": ["Elephant"]},
+            {"question": "What is the largest reptile?", "answer": ["Crocodile"]},
+            {"question": "What is the largest mammal?", "answer": ["Blue whale"]},
+            {"question": "What is the largest bird?", "answer": ["Ostrich"]},
+            {"question": "What is the strongest animal relative to its size?", "answer": ["Dung beetle", "Beetle"]},
+            {"question": "What is the fastest land animal?", "answer": ["Cheetah"]},
+            {"question": "What animal has 3 hearts?", "answer": ["Octopus", "Squid", "Cuttlefish"]},
+            {"question": "What is the sleepiest animal?", "answer": ["Koala"]},
+            {"question": "What is the only bird that can fly backwards?", "answer": ["Hummingbird"]},
+            {"question": "What is the only mammal capable of flight?", "answer": ["Bat"]},
+            {"question": "Out of the following 4 animals, which can hold their breath the longest underwater? A) Dolphin B) Orca C) Seal D) Sloth", "answer": ["D", "Sloth", "D) Sloth"]},
+            {"question": "What animal laughs when tickled?", "answer": ["Rat"]},
+            {"question": "What is the only animal to have cube shaped poop?", "answer": ["Wombat"]},
+            {"question": "What mammal has the strongest bite force?", "answer": ["Hippopotamus", "Hippo"]}
+        ]
 
 
-#A loop that randomly goes through the questions and asks for an answer, adding one to the score if right, not adding anything if wrong.
+        #shuffles the question so they are in a random order every time
+        random.shuffle(questions)
 
-for q in questions:
-    user_answer = input(q["question"] + " ")
-    if user_answer.lower().strip() in [str(a).strip().lower() for a in q["answer"]]:
-        score += 1
-        print("Correct!")
-    else:
-        print("Wrong")
+        score = ask_questions(questions)
 
-#final score
-print("Your final score is:", score, "out of", len(questions))
+        #final score
+        print("Your final score is:", score, "out of", len(questions))
 
 
-#checks if user wants to know answers
-know_ans = input("Do you want to know the answers? (yes/no) ")
-if know_ans.lower() == "yes":
-    for q in questions:
-        print(q["question"], "-", q["answer"])
+        #checks if user wants to know answers
+        know_ans = input("Do you want to know the answers? (yes/no) ")
+        if know_ans.lower() == "yes":
+            for q in questions:
+                print(q["question"], "-", q["answer"])
 
-print("Thanks for playing!")
+        #checks if user wants to play again
+        play_again = input ("\nplay again (yes/no) ")
+        if play_again.lower().strip() != "yes":
+            break
+
+        print("\nThanks for playing!")
+
+
+
+#A function for a loop that randomly goes through the questions and asks for an answer, adding one to the score if right, not adding anything if wrong.
+def ask_questions(questions_list):
+    score=0
+    for q in questions_list:
+        user_answer = input(q["question"] + " ")
+        if user_answer.lower().strip() in [str(a).strip().lower() for a in q["answer"]]:
+            score += 1
+            print("Correct!")
+        else:
+            print("Wrong")
+    return score
+
+#runs the program that was defined in main function
+main()
+
